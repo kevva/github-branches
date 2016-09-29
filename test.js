bcd19a3375047da31c8d2cfcf2a69a1c201bdf99
Kevin Martensson <kevinmartensson@gmail.com>
2016-09-29 22:54:50 +0200

    Add type check test

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 import test from 'ava';
 import m from './';
 
-test(async t => {
+test('get branches', async t => {
 	const data = await m('kevva/github-branches');
 	t.truthy(data.length);
 	t.is(data[0].name, 'master');
+});
+
+test('accepts a string', async t => {
+	t.throws(m(0), 'Expected a `string`, got `number`');
 });
